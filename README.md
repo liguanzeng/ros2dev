ac8fcd643cefd0221b7c8df75b053fc772898677
liguanzeng <1245842028@qq.com>
2023-06-04 22:18:19 +0800

    add parameters example

--- README.md
+++ README.md
@@ -8,4 +8,5 @@
 - src目录内为源码
 - install目录为编译好的pkg
 ## 常用命令
+- `ros2 pkg create example_parameters_rclcpp --build-type ament_cmake --dependencies rclcpp` ws下执行，创建功能包
 - `ros2 run package-name node-name` ros2运行某个包下面的某个节点
---
+++ src/example_parameters_rclcpp/src/parameters_basic.cpp
@@ -0,0 +1,48 @@
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
+/*
+    # declare_parameter            声明和初始化一个参数
+    # describe_parameter(name)  通过参数名字获取参数的描述
+    # get_parameter                通过参数名字获取一个参数
+    # set_parameter                设置参数的值
+*/
+class ParametersBasicNode : public rclcpp::Node {
+ public:
+  // 构造函数,有一个参数为节点名称
+  explicit ParametersBasicNode(std::string name) : Node(name) {
+    RCLCPP_INFO(this->get_logger(), "节点已启动：%s.", name.c_str());
+    this->declare_parameter("rcl_log_level", 0);     /*声明参数*/
+    this->get_parameter("rcl_log_level", log_level); /*获取参数*/
+    /*设置日志级别*/
+    this->get_logger().set_level((rclcpp::Logger::Level)log_level);
+    using namespace std::literals::chrono_literals;
+    timer_ = this->create_wall_timer(
+        1000ms, std::bind(&ParametersBasicNode::timer_callback, this));
+  }
+
+ private:
+  int log_level;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  void timer_callback() {
+    this->get_parameter("rcl_log_level", log_level); /*获取参数*/
+    /*设置日志级别*/
+    this->get_logger().set_level((rclcpp::Logger::Level)log_level);
+    std::cout<<"======================================================"<<std::endl;
+    RCLCPP_DEBUG(this->get_logger(), "我是DEBUG级别的日志，我被打印出来了!");
+    RCLCPP_INFO(this->get_logger(), "我是INFO级别的日志，我被打印出来了!");
+    RCLCPP_WARN(this->get_logger(), "我是WARN级别的日志，我被打印出来了!");
+    RCLCPP_ERROR(this->get_logger(), "我是ERROR级别的日志，我被打印出来了!");
+    RCLCPP_FATAL(this->get_logger(), "我是FATAL级别的日志，我被打印出来了!");
+  }
+};
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  /*创建对应节点的共享指针对象*/
+  auto node = std::make_shared<ParametersBasicNode>("parameters_basic");
+  /* 运行节点，并检测退出信号*/
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
